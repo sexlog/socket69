24eeb71623eec46d8251f6f2ad9cf62a5a83f8dc
Alex Figueiredo <alexxfsilva@gmail.com>
2016-12-18 14:35:30 -0200

    Gulp e workflow

---
+++ gulpfile.babel.js
@@ -0,0 +1,50 @@
+const gulp       = require('gulp'),
+      sourcemaps = require('gulp-sourcemaps'),
+      source     = require('vinyl-source-stream'),
+      buffer     = require('vinyl-buffer'),
+      browserify = require('browserify'),
+      watchify   = require('watchify'),
+      babel      = require('babelify');
+
+function compile(watch) {
+
+    let bundler = watchify(browserify('./src/main.js', {
+        debug: true
+    }).transform(babel));
+
+    function rebundle() {
+        bundler.bundle()
+               .on('error', function(err) {
+                   console.error(err);
+                   this.emit('end');
+               })
+               .pipe(source('build.js'))
+               .pipe(buffer())
+               .pipe(sourcemaps.init({loadMaps: true}))
+               .pipe(sourcemaps.write('./'))
+               .pipe(gulp.dest('./dist'));
+    }
+
+    if (watch) {
+        bundler.on('update', function() {
+            console.log('Recompilado');
+            rebundle();
+        });
+    }
+
+    rebundle();
+}
+
+function watch() {
+    return compile(true);
+}
+
+gulp.task('build', function() {
+    return compile();
+});
+
+gulp.task('watch', function() {
+    return watch();
+});
+
+gulp.task('default', ['watch']);
---
+++ src/main.js
@@ -0,0 +1,6 @@
+class Socket69 {
+
+    constructor() {
+        this.version = '1.1.0';
+    }
+}
